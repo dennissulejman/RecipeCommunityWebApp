116f590a9c69c48aa9f367611398e4690f72ba7f
dennissulejman <45164504+dennissulejman@users.noreply.github.com>
2020-07-11 11:56:29 +0200

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RecipeCommunityWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
