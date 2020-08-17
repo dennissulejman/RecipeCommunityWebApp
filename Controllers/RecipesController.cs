36ed10f3b01d874a5190606447a53daadbc07656
Dennis Sulejman <dennis_s93@hotmail.com>
2020-08-17 11:26:18 +0200

    Transfer between computers, changed the layout slightly but still needs work.

---
+++ Controllers/RecipesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RecipeCommunityWebApp.Controllers
+{
+    public class RecipesController : Controller
+    {
+        private readonly ILogger<RecipesController> _logger;
+
+        public RecipesController(ILogger<RecipesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult RecipeBrowser()
+        {
+            return View();
+        }
+
+        public IActionResult RecipeSearch(string recipeSearchInput)
+        {
+            ViewData["RecipeSearchInput"] = recipeSearchInput;
+
+            return View();
+        }
+    }
+}
